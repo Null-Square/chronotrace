e7b6b56beb703e8f3cb4ff2c583fe3e0d1869385
Omar Tawil <omar.y.altawil@gmail.com>
2026-08-29 00:13:01 +0300

    feat: export chronology identifiability diagnostics

--- src/chronotrace/geometry/__init__.py
+++ src/chronotrace/geometry/__init__.py
@@ -12,15 +12,25 @@ from chronotrace.geometry.commutator import (
     pairwise_endpoint_geometry,
     permutation_signature,
 )
+from chronotrace.geometry.identifiability import (
+    ChronologyIdentifiability,
+    bracket_matrix,
+    chronology_identifiability,
+    normalized_remainder_ratio,
+)
 
 __all__ = [
+    "ChronologyIdentifiability",
     "PairwiseGeometry",
     "PermutationDecode",
+    "bracket_matrix",
+    "chronology_identifiability",
     "decode_pairwise_order",
     "decode_permutation",
     "estimate_step_size",
     "local_stage_derivatives",
     "multi_stage_symmetric_reference",
+    "normalized_remainder_ratio",
     "pairwise_chrono_score",
     "pairwise_endpoint_geometry",
     "permutation_signature",
